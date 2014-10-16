fa6d56ededab2e8ea114ada37bdf839dcda7865e
scutLaoYi <scutLaoYi@gmail.com>
2014-10-16 17:11:21 +0800

    Just a new button for adding new tag.

--- application/views/tags/index.php
+++ application/views/tags/index.php
@@ -1,7 +1,12 @@
 <div class="well well-lg">
 <h2>All tags</h2>
 </div>
-
+<div class="list-group">
+    <form role="form" name="new" action="/tags/add" method="POST">
+        <div class="form-group"></div>
+        <button type="submit" class="btn btn-primary">Add</button>
+    </form>
+</div>
 <div class="list-group">
 <?php foreach ($this->tags as $tag) {?>
     <a class="list-group-item" href="/tags/edit/<?php echo $tag->id?>"><?php echo $tag->name;?></a>
