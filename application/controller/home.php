84091f4de17dcdb4529a78fabbb5945d66dbdd17
scutLaoYi <scutLaoYi@gmail.com>
2014-10-20 14:36:35 +0800

    Update : remove some trash.

--- application/controller/home.php
+++ application/controller/home.php
@@ -20,33 +20,4 @@ class Home extends Controller
 
         $this->render('home/index.php');
     }
-
-    /**
-     * PAGE: exampleone
-     * This method handles what happens when you move to http://yourproject/home/exampleone
-     * The camelCase writing is just for better readability. The method name is case insensitive.
-     */
-    public function exampleOne()
-    {
-        $title = 'home example one';
-        // load views. within the views we can echo out $songs and $amount_of_songs easily
-        require 'application/views/_templates/header.php';
-        require 'application/views/home/example_one.php';
-        require 'application/views/_templates/footer.php';
-    }
-
-    /**
-     * PAGE: exampletwo
-     * This method handles what happens when you move to http://yourproject/home/exampletwo
-     * The camelCase writing is just for better readability. The method name is case insensitive.
-     */
-    public function exampleTwo()
-    {
-        // debug message to show where you are, just for the demo
-        echo 'Message from Controller: You are in the controller home, using the method exampleTwo()';
-        // load views. within the views we can echo out $songs and $amount_of_songs easily
-        require 'application/views/_templates/header.php';
-        require 'application/views/home/example_two.php';
-        require 'application/views/_templates/footer.php';
-    }
 }
