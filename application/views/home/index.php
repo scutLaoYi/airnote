47b416904378c2f5876795a4aef5339256e1db21
scutLaoYi <scutLaoYi@gmail.com>
2014-11-01 16:37:51 +0800

    Update: home page~

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,6 +1,22 @@
-<div class="container"> 
-    <div class="starter-template">
-        Yep. This is my online notebook.
+<div class="jumbotron"> 
+    <h1>Airnote Project</h1>
+
+    <br/>
+    <div class="btn-toolbar">
+    <div class="btn-group">
+        <a class="btn btn-primary" role="button" href="http://www.scutlaoyi.tk">My blog</a>
     </div>
-    <a href="http://www.scutlaoyi.tk">View my blog site.</a>
+    <div class="btn-group">
+        <a class="btn btn-success" role="button" href="/user/login">Login</a>
+    </div>
+    <div class="btn-group">
+        <a class="btn btn-danger" role="button" href="/notes/index">Notes</a>
+    </div>
+    <div class="btn-group">
+        <a class="btn btn-info" role="button" href="/tags/index">Tags</a>
+    </div>
+    <div class="btn-group">
+        <a class="btn btn-warning" role="button" href="#">Try it</a>
+    </div>
+    </div>
 </div>
