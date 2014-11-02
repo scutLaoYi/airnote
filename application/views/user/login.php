54e761e5e91d17a6492bb58498b9f35a70ce3ec5
scutLaoYi <scutLaoYi@gmail.com>
2014-11-02 10:01:38 +0800

    Update: Login page now better than before

--- application/views/user/login.php
+++ application/views/user/login.php
@@ -1,13 +1,36 @@
+<div class="container-fluid">
+
+<div class="row">
+<div class="col-md-3">
+</div>
+<div class="col-md-6">
+<div class="panel panel-primary">
+<div class="panel-heading">
+    Login
+</div>
+<div class="panel-body">
 <form role="form" name="input" action="/user/login" method="POST">
-    <div class="form-group">
-    <label for="input_text">Username:</label>
+    <div class="input-group">
+    <span class="input-group-addon">Username</span>
     <input type="text" class="form-control" id="username" name="username" placeholder="Tell me who you are">
-    <label for="input_text">Password:</label>
-    <input type="password" class="form-control" id="password" name="password" placeholder="Tell me your password">
-    <label for="input_text">2-Step-auth:</label>
-    <input type="text" class="form-control" id="password" name="two_fa_code" placeholder="Tell me your password">
     </div>
-    <button type="submit" class="btn btn-default">Submit</button> 
+    <br/>
+    <div class="input-group">
+    <span class="input-group-addon">Password</span>
+    <input type="password" class="form-control" id="password" name="password" placeholder="Tell me your password">
+    </div>
+    <br/>
+    <div class="input-group">
+    <span class="input-group-addon">Auth</span>
+    <input type="text" class="form-control" id="password" name="two_fa_code" placeholder="Tell me your two step auth">
+    </div>
+    <br/>
+    <button type="submit" class="btn btn-default">Login</button> 
 </form>
-
-
+</div>
+</div>
+</div>
+<div class="col-md-3">
+</div>
+</div>
+</div>
